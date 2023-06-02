36694acff8eddd7d0f44397f1b9ae1c63cfac203
pornimashejul21 <pornimashejul2001@gmail.com>
2023-06-02 12:22:49 +0530

    new changes

--- Session16/script.js
+++ Session16/script.js
@@ -22,3 +22,35 @@
 
 //     document.getElementById('powerResult').innerHTML = "Power of" + base + ": " + power;
 // }
+
+
+
+
+// function calculateLeaves() {
+
+//     var startDateString = document.getElementById("startDate").value;
+//     var endDateString = document.getElementById("endDate").value;
+
+//     var startDate = new Date(startDateString);
+//     var endDate = new Date(endDateString);
+
+//     var milisecondsPerDay = 24 * 60 * 60 * 1000;
+
+//     var diffInDays = Math.round((endDate - startDate) / milisecondsPerDay);
+//     var leavesCount = diffInDays + 1;
+
+//     document.getElementById("leavesCount").innerHTML = "Total number of leaves :" + leavesCount;
+// }
+
+
+// // Adding the item
+// localStorage.setItem("Name", "iqra");
+
+// // Retrieving the current value
+// let currentValue = localStorage.getItem("Name");
+// console.log("Current value:", currentValue);
+
+// // Replacing the value
+// localStorage.setItem("Name", "Technology");
+// console.log("Updated value:", localStorage.getItem("Name"));
+// </script>
